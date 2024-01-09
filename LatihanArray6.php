074f124a1d9db784e6bec42b1be897810c74a78f
Retr0 <148172448+MotivateBro@users.noreply.github.com>
2024-01-09 09:32:33 +0700

    Add files via upload

---
+++ LatihanArray6.php
@@ -0,0 +1,13 @@
+<?php
+//Contoh Array
+$cars = array("SupraX","RGX","R15","MIO");
+$negara = ["Cimahi", "Ditempat", "Yamaha"];
+$kota = array("Aceh","Sydney","Melbourn","Bandung");
+
+//menampilkan
+echo "I Suka " . $cars[0] . ", " . $cars[1] . " Dan " . $cars[2] . "." ;
+echo "<br>";
+var_dump($negara);
+echo "<br>";
+print_r($kota)
+?>
---
+++ LatihanArray7.php
@@ -0,0 +1,14 @@
+<?php
+$cars = array("SupraX","RGX","R15","MIO");
+$negara = ["Cimahi", "Ditempat", "Yamaha"];
+$kota = array("Aceh","Sydney","Melbourn","Bandung");
+
+// menambah
+var_dump($kota);
+$kota[] = "Tanah Abang";
+echo "<br>";
+$kota[4] = "Nangor fr fr";
+echo "<br>";
+var_dump($kota);
+
+?>
